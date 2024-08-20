67a2067497f29442979d570bbbc671dd7f3fcec8
Dmitry Bikman <dmitry@bikman.ru>
2024-08-21 02:47:38 +0300

    Added statusCode property to APIError class

--- src/error.ts
+++ src/error.ts
@@ -3,11 +3,13 @@ import {APIErrorType} from './dtos/errors.js'
 export class APIError extends Error {
     public readonly requestId: string;
     public readonly code: number;
+    public readonly statusCode: number | undefined;
 
-    constructor(apiError: APIErrorType) {
+    constructor(apiError: APIErrorType, statusCode: number) {
         super(apiError.error);
         this.requestId = apiError.requestId;
         this.code = apiError.code;
+        this.statusCode = statusCode;
         Error.captureStackTrace(this, APIError);
     }
 }
